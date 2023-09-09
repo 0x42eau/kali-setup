cb94ce16899d7a4b9a035d07c8834cae3ed0d806
cheesemonkey00 <49952735+cheesemonkey00@users.noreply.github.com>
2023-09-09 14:29:22 -0600

    Update set-me-up.sh

--- set-me-up.sh
+++ set-me-up.sh
@@ -4,16 +4,47 @@ apt update -y
 apt upgrade -y
 
 sleep 3
+
+cd /opt/
+git clone https://github.com/Dewalt-arch/pimpmykali.git
+cd /opt/pinkmykali
+chmod +x pimpmykali.sh
+
+echo ''
+echo ''
+sleep 1h
+echo '' 
+echo ''
+echo 'sleeping for 30m to give time to run pimpmykali manually --  /opt/pimpmykali.sh'
+echo '' 
+echo ''
+
 echo 'getting scripts'
 cd /opt && git clone https://github.com/cheesemonkey00/scripts.git
 
-mv /home/kali/.bashrc /home/kali/.bashrc.bak
+#mv /home/kali/.bashrc /home/kali/.bashrc.bak
 
-cp /opt/scripts/kali-bashrc /home/kali/.bashrc
+#cp /opt/scripts/kali-bashrc /home/kali/.bashrc
 
-source ~/.bashrc
+#source ~/.bashrc
+cd /opt
+git clone https://github.com/cheesemonkey00/EZ-Crackmap.git
+cd /opt/EZ-Crackmap
+chmod +x 6oh-install-new.sh
+./6oh-install-new.sh
+
+echo ''
+echo ''
+sleep 5
+echo ''
+echo ''
 
 apt install neo4j && apt install bloodhound
+cd /opt
+git clone https://github.com/PlumHound/PlumHound.git
+cd /opt/PlumHound/
+pip3 install -r requirements.txt
+
 
 #greps
 #alias grepi='grep -ai'
